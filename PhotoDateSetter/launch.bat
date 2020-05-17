6bb185db3d6c62af5a90f2ef20798357cd6f7a7e
gupichon <gupichon@gmail.com>
2020-05-17 15:11:36 +0200

    Correction du lanceur windows

--- PhotoDateSetter/launch.bat
+++ PhotoDateSetter/launch.bat
@@ -1,2 +1,2 @@
-start \B image\bin\java.exe -jar PhotoDateSetter-0.0.1-SNAPSHOT-jar-with-dependencies.jar
-
+start /B image\bin\javaw.exe -jar PhotoDateSetter-0.0.1-SNAPSHOT-jar-with-dependencies.jar
+exit
